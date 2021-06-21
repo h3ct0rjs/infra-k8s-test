dc2797b0dfafaa316af0c90704da4934a76b9c13
Hector F. Jimenez <hfjimenez@utp.edu.co>
2021-06-21 16:02:31 -0500

    Add DigitalOcean Provider latest version along with the token specification

---
+++ do-infrastructure/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.9.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token_h
+}
